c1c39ef404484084586429470b87ef5ff8d5982f
lidiyastepanenko <lidiyka@gmail.com>
2017-02-03 16:40:44 +0200

    localstorage, add date

--- taskList/controller.js
+++ taskList/controller.js
@@ -1,28 +1,38 @@
 'use strict';
 angular.module('todoApp.controller', [])
 .controller('todoController', ["$scope", function($scope){
-	$scope.newTask = "";
-	$scope.taskList = [
-		{description: "Купить билеты на самолет", done:false},
-		{description: "Забронировать место в гостинице", done:false},
-		{description: "Отдохнуть!!!", done:false},
-	];
+	$scope.setDate= new Date ();
+	$scope.taskList = [];
+        console.log($scope.taskList);
+        if (localStorage.object){
+                $scope.taskList = JSON.parse(localStorage.getItem("object"));
+                console.log($scope.taskList);
+                console.log(new Date ());
+            }
 	$scope.addTask = function(){
-		if ($scope.newTask != ""){
-			$scope.taskList.push({description: $scope.newTask, done : false});
-			$scope.newTask = "";
-		}
-		return $scope.newTask = "";		
+		$scope.taskList.push({description: $scope.newTask, date: $scope.setDate, done : false})
+		return $scope.newTask ="", $scope.setDate = new Date ();	
 	};	
+	$scope.remain = function () {
+                var count = $scope.taskList.length;
+                angular.forEach($scope.taskList, function(task) {
+                    var sObj = JSON.stringify($scope.taskList);
+                    localStorage.setItem("object", sObj);
+                    count -= task.done;
+                    console.log(sObj);
+                });
+            return count;
+        };
 	$scope.deleteTask = function(index){
 		$scope.taskList.splice(this.$index, 1);
 	};
 	$scope.editTask = function(task) {
-    	$scope.activeTask = task;
+		$scope.activeTask = task;
     	console.log($scope.activeTask);
 	}; 
 	$scope.updateTask = function(task) { 
     	$scope.activeTask = task.description; 
+    	console.log(task.description)
 	};
 	
 }]);
